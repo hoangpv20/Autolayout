d78401ff63b90dd82e62baa19b8af60001fcb8ae
hoangpv20 <117055504+hoangpv20@users.noreply.github.com>
2022-12-05 17:07:35 +0700

    Add files via upload

---
+++ ListOfTasksFinal/ListOfTasksFinal/CustomCell/GridCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  GridCollectionViewCell.swift
+//  ListOfTasksFinal
+//
+//  Created by admin on 24/11/2022.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var mainText: UILabel!
+    @IBOutlet weak var subText: UILabel!
+    @IBOutlet weak var trashButton: UIButton!
+    @IBOutlet weak var checkButton: UIButton!{
+        didSet{
+            checkButton.setImage(UIImage(named:"whiteUncheck"), for: .normal)
+            checkButton.setImage(UIImage(named:"whiteCheck"), for: .selected)
+        }
+    }
+}
---
+++ ListOfTasksFinal/ListOfTasksFinal/CustomCell/SearchCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SearchCollectionViewCell.swift
+//  ListOfTasksFinal
+//
+//  Created by admin on 24/11/2022.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var checkButton: UIButton!{
+        didSet{
+            checkButton.setImage(UIImage(named:"whiteUncheck"), for: .normal)
+            checkButton.setImage(UIImage(named:"whiteCheck"), for: .selected)
+        }
+    }
+    @IBOutlet weak var trashButton: UIButton!
+    @IBOutlet weak var mainText: UILabel!
+    @IBOutlet weak var subText: UILabel!
+}
